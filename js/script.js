a8027183bf09b6dfacd1c7123839da57b18da46f
“yoyochan325” <chanmaniao@gmail.com>
2023-07-03 11:20:04 +0100

    switchtext

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var textArray = ["Magnificance. Intelligence. Courage.", "Man Iao Chan"];
+function switchText() {
+    var textElement = document.getElementById("textswitcher");
+    var currentIndex = textArray.indexOf(textElement.innerHTML);
+    var newIndex = (currentIndex +1)% textArray.length;
+    textElement.innerHTML=textArray[newIndex];
+}
+setInterval(switchText,3000);
