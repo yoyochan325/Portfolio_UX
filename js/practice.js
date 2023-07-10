27ce322d4d10282eb18397e5606b482bfb05640d
“yoyochan325” <chanmaniao@gmail.com>
2023-07-10 11:38:24 +0100

    set themes

---
+++ js/practice.js
@@ -0,0 +1,30 @@
+let x;
+x = 5;//assign value 5 to the variable x
+
+let y = 10; //declare a varaible y and assign 10
+
+let name = 'Yoyo Chan'; //assign string value to the variable
+let isStudent = true; //assign a boolean value to the variable 
+
+let school = 'UCL';
+
+let introduction = `My name is ${name}. I study at ${school}`; 
+//use backticks to include variables in a string
+
+let message = "My name is " + name + " and I study at " + school + ".";
+//use double quote and use the plus sign to 
+//add variable in a string, namely concatenating variables in a string
+
+//round down division
+let dividend = 13;
+let divisor = 3;
+let result = Math.floor(dividend / divisor);
+let modulo = dividend % divisor;
+//modulo is the remainder, while round down is the integer in a division
+
+let number = 25;
+let squareRoot = Math.sqrt(number);
+
+let number = 24;
+let squareRoot = Math.sqrt(number);
+let roundedSquareRoot= Math.round(squareRoot);
--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 var textArray = ["Magnificance. Intelligence. Courage.", "Man Iao Chan"];
+//the index for the first set of words is 0, while the index for the 2nd set is 1
 function switchText() {
     var textElement = document.getElementById("textswitcher");
-    var currentIndex = textArray.indexOf(textElement.innerHTML);
+    var currentIndex = textArray.indexOf(textElement.innerHTML); //current index = 0 at the beginning 
     var newIndex = (currentIndex +1)% textArray.length;
+    //Since current index is 0, we add 1 and modulo 2
     textElement.innerHTML=textArray[newIndex];
+    //the HTML words need to be changed to the text array set of the new index
 }
 setInterval(switchText,2000);
+
+function setTheme() {
+    var currentHour = new Date().getHours();
+    var bodyElement = document.getElementsByTagName("body")[0]; 
+    //[0] indicates the first <body> tag in the HTML file,
+    //which is the opening of <body> instead of </body>
+    if (currentHour >= 6 && currentHour < 18){
+        bodyElement.classList.remove("body-dark-mode");
+        bodyElement.classList.add("body-light-mode");
+    }else{
+        bodyElement.classList.remove("body-light-mode");
+        bodyElement.classList.add("body-dark-mode");
+    }
+    }
+setTheme();
+
